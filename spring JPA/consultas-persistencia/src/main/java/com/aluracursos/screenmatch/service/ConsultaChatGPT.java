e3fe184ab674322230cfe9cee3fa71694dcc32d1
Alexis <mamanialexis29@gmail.com>
2025-12-15 02:02:56 -0300

    jpa basico completado

---
+++ spring JPA/consultas-persistencia/src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
@@ -0,0 +1,24 @@
+package com.aluracursos.screenmatch.service;
+
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
+
+public class ConsultaChatGPT {
+    public static String obtenerTraduccion(String texto) {
+        String apiKey = System.getenv("OPENAI_API_KEY");
+        if (apiKey == null || apiKey.isEmpty()) {
+            throw new IllegalStateException("La variable de entorno OPENAI_API_KEY no está configurada");
+        }
+        OpenAiService service = new OpenAiService(apiKey);
+
+        CompletionRequest requisicion = CompletionRequest.builder()
+                .model("gpt-3.5-turbo-instruct")
+                .prompt("traduce a español el siguiente texto: " + texto)
+                .maxTokens(1000)
+                .temperature(0.7)
+                .build();
+
+        var respuesta = service.createCompletion(requisicion);
+        return respuesta.getChoices().get(0).getText();
+    }
+}
